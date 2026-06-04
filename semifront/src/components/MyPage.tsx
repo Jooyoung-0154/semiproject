5bfca6e0377ed1767441a52e34b1026dd30d2562
정민기 <daekook2000@naver.com>
2026-06-04 17:59:40 +0900

    스크랩 오류 수정

--- semifront/src/components/MyPage.tsx
+++ semifront/src/components/MyPage.tsx
@@ -5,6 +5,8 @@ import {
   Users,
   MessageSquare,
   DollarSign,
+  ChefHat,
+  Clock,
 } from "lucide-react";
 import RecipeCard from "./RecipeCard";
 import "./MyPage.css";
@@ -858,8 +860,13 @@ export default function MyPage() {
                         : null;
 
                       const levelColor =
-                        LEVEL_COLOR[recipe.levelNm] ??
-                        "bg-gray-100 text-gray-600";
+                        recipe.levelNm === "상"
+                          ? "bg-red-100 text-red-700"
+                          : recipe.levelNm === "중"
+                            ? "bg-yellow-100 text-yellow-700"
+                            : recipe.levelNm === "하"
+                              ? "bg-green-100 text-green-700"
+                              : "bg-gray-100 text-gray-600";
 
                       return (
                         <div
